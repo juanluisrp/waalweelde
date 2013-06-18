50b430683ace4501c786c6c0f11fb8df61dc6a95
Steven Ottens <github@minst.net>
2013-06-18 13:52:32 +0200

    Graphs in a popup

--- script/graph.js
+++ script/graph.js
@@ -2,6 +2,12 @@
 $.template('urdGraphTree',
 '<div class="urd-graphtree  ui-widget-content"></div>');
 
+$.template('urdGraphElement',
+'<div class="urd-graphelement">${title}</div>');
+
+$.template('urdGraphPopup',
+'<div class="urd-graphpopup" id="${id}"><svg></svg></div>');
+
 $.widget("urd.urdGraphs", {
   options: {
     urd: undefined,
@@ -11,87 +17,60 @@ $.widget("urd.urdGraphs", {
     var urd;
     var self = this;
     var element = this.element;
-    this._createWaterGraph({url:'Definitief Ontwerp_results_1D.csv'});
+    var lmElement = $.tmpl('urdGraphTree').appendTo(element);
+    urd = $(this.options.urd).data('urd');
+    //TODO: hier moet dus aan de hand van de model run grafieken worden gegenereerd ofzo
+       
+    this._createWaterGraph({url:'Definitief Ontwerp_results_1D.csv', widget: lmElement});
   },
   _createWaterGraph: function(options) {
+    var widget = options.widget;
+    var title= 'Definitief Ontwerp';
+    var button = $.tmpl('urdGraphElement', {title: title}).appendTo(widget);
+    
+    var id = "urdGraph-" + new Date().getTime();
+   
+   var html = $.tmpl('urdGraphPopup', {id: id});
+    
+    var dialog =  $('<div id="btn-'+id+'"></div>').html(html).dialog({title: title});
+     widget.delegate('.urd-graphelement', 'click', function() {
+            dialog.dialog('isOpen')?dialog.dialog('close'):dialog.dialog('open')
+           
+            return false;
+        });
     var url = options.url;
     var color = d3.scale.category10();
-    var width = 200;
-    var height= 141;
-    var x = d3.scale.linear().range([0, width]);
-    var y = d3.scale.linear().range([height, 0]);
-    
-    var xAxis = d3.svg.axis()
-      .scale(x)
-      .orient("bottom");
-
-    var yAxis = d3.svg.axis()
-      .scale(y)
-      .orient("left");
-
-    var line = d3.svg.line()
-      .interpolate("basis")
-      .x(function(d) {
-      return x(d.lengte); 
-      })
-      .y(function(d) { return y(d.hoogte); });
-      
-    var svg = d3.select('#'+this.element.attr('id')).append("svg")
-      .attr("width", width )
-      .attr("height", height)
-      .append("g");
-
-    
+    var meetpunten;
     d3.csv(url, function(error,data) {
       color.domain(d3.keys(data[0]).filter(function(key) { return key !== ('location'); }));   
 
      data.forEach(function(d) {
         d.location = parseFloat(d.location);
       });      
-      var meetpunten = color.domain().map(function(name) {
+      meetpunten = color.domain().map(function(name) {
         return {
-          name: name,
+          key: name,
           values: data.map(function(d) {
-            return {lengte: d.location, hoogte: +d[name]};
+            return {x: d.location, y: +d[name]};
           })
         };
       });
-   
-   
-   x.domain(d3.extent(data, function(d) { return d.location; }));
-
-    y.domain([
-      d3.min(meetpunten, function(c) { return d3.min(c.values, function(v) { return v.hoogte; }); }),
-      d3.max(meetpunten, function(c) { return d3.max(c.values, function(v) { return v.hoogte; }); })
-    ]);
-    svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis);
-
-  svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
-
-      
-    var meetpunt = svg.selectAll(".meetpunt")
-      .data(meetpunten)
-      .enter().append("g")
-      .attr("class", "meetpunt");
-
-    meetpunt.append("path")
-        .attr("class", "line")
-        .attr("d", function(d) { return line(d.values); })
-        .style("stroke", function(d) { return color(d.name); });
-
+     
     
-    meetpunt.append("text")
-      .datum(function(d) { return {name: d.name, value: d.values[d.values.length - 1]}; })
-      .attr("transform", function(d) { return "translate(" + x(d.value.lengte) + "," + y(d.value.hoogte) + ")"; })
-      .attr("x", 3)
-      .attr("dy", ".35em")
-      .text(function(d) { return d.name; });
+    nv.addGraph(function(){
+      var chart = nv.models.lineChart();
+      chart.xAxis;
+      
+      chart.yAxis;
+      
+      d3.select('#'+id+' svg')
+        .datum(meetpunten)
+        .call(chart);
+       nv.utils.windowResize(function() { d3.select('#'+id+' svg').call(chart) });
     });
+    
+     });
+   
   }
   
 });
